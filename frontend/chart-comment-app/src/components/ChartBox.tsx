564cab9012d71a7673162f735896c2bfc99c53c6
Owen Tourlamain <owen.tourlamain@gmail.com>
2022-09-17 00:02:02 +0100

    Add comment count to data points (faked count). Progresses #1

--- frontend/chart-comment-app/src/components/ChartBox.tsx
+++ frontend/chart-comment-app/src/components/ChartBox.tsx
@@ -12,11 +12,25 @@ type DataPoint = {
   }
 
 const ChartBox = ({ data, onClick }: Props) => {
+    function renderDot(props: any) {
+        console.log(props)
+        const x = props.cx;
+        const y = props.cy;
+        const commentCount = props.payload.commentCount;
+        const key = props.payload.x;
+        return (
+            <svg className='chartDot' x={x - 20} y={y - 20} height={40} width={40}>
+                <circle cx="20" cy="20" r="10" fill="blue"></circle>
+                <text x="20" y="22" text-anchor="middle" alignment-baseline="middle" fill="white">{commentCount}</text>
+            </svg>
+        )
+    }
+
     return (
         <div className='chartBox'>
             <ResponsiveContainer width="90%">
                 <LineChart data={data} onClick={(e) => onClick(e.activeLabel)} >
-                    <Line type={"monotone"} dataKey={"y"} stroke={"#8884d8"} />
+                    <Line type={"monotone"} dataKey={"y"} stroke={"#8884d8"} dot={renderDot}/>
                     <XAxis dataKey={"x"} />
                     <YAxis dataKey={"y"} />
                     <Label value="Test" />
