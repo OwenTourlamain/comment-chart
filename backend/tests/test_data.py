25e77373e210ebb1c70b9cbef5d2290b78347576
Owen Tourlamain <owen.tourlamain@gmail.com>
2022-09-17 21:27:01 +0100

    Initial testing structure. Progresses #2

---
+++ backend/tests/test_data.py
@@ -0,0 +1,11 @@
+def test_get_data():
+    pass
+
+def test_get_comments():
+    pass
+
+def test_add_comment():
+    pass
+
+def test_initialise_db():
+    pass
---
+++ backend/tests/test_main.py
@@ -0,0 +1,8 @@
+def test_get_comments():
+    pass
+
+def test_add_comment():
+    pass
+
+def test_get_data():
+    pass
